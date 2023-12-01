95ae0d0428c7f6ebb0073e0521c9e557953b73b7
giuseppevignanello <peppe.vignanello@gmail.com>
2023-12-01 11:35:15 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
-<template>
-  <div>
-    <h1 class="bg-danger">Prova</h1>
-  </div>
-</template>
 
-<script>
-export default {
-  setup() {
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AppHeader from "./components/AppHeader.vue";
+import axios from 'axios';
 
+export default defineComponent({
+  components: {
+    AppHeader
+  },
+  data() {
 
     return {
+      apiUrlBase: "https://www.googleapis.com/books/v1/volumes?q=",
+      apiKey: "AIzaSyBJRWR4g442X0LJRco_-qyM-c2tAx_hMqs",
 
-    }
-  }
-}
+    };
+  },
+});
 </script>
+
+
+<template>
+  <div>
+    <AppHeader></AppHeader>
+  </div>
+</template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    name: "AppHeader",
+    props: {
+
+    },
+    components: {
+
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<template>
+    <header
+        class="bg-dark d-flex flex-wrap align-items-center justify-content-center justify-content-md-around py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-decoration-none">
+            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                <use xlink:href="#bootstrap" />
+            </svg>
+        </a>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="#" class="nav-link px-2 text-white">Home</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+        </ul>
+
+        <div class="col-md-3 text-end">
+            <button type="button" class="btn btn-outline-light me-2">Login</button>
+            <button type="button" class="btn btn-light">Sign-up</button>
+        </div>
+    </header>
+</template>
+<style scoped></style>
